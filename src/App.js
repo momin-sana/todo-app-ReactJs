0d94b79fb49d576fe9a09b40b525684e0d0aaddc
sanamomin17 <sana.m.momin@outlook.com>
2021-10-15 14:03:09 +0500

    Image input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     return ( <
         div className = "App" >
         LEARN REACT AND REACT NATIVE <
+        img src = "{https://cdn.pixabay.com/photo/2015/06/08/15/11/typewriter-801921_960_720.jpg}"
+        alt = "Logo" / >
+        <
         /div>
     );
 }
