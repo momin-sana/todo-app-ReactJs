e5dd0bd08ac9f1cbe916a4e595a757a4d7437cc7
sanamomin17 <sana.m.momin@outlook.com>
2021-10-15 14:05:24 +0500

    Image Link updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return ( <
         div className = "App" >
         LEARN REACT AND REACT NATIVE <
-        img src = "{https://cdn.pixabay.com/photo/2015/06/08/15/11/typewriter-801921_960_720.jpg}"
+        img src = "{https://pixabay.com/get/ge873844f34f4e47196679269a901b98ba85ca2ad6ee26512cf93f26df8f964e7a85a064237e8ee1c8b19756e4ba4e78f119d4916379cd090c757bf4fd39e4e31e1dfbbbcab6ebbea3f9a2c8dc757d64c_1920.jpg}"
         alt = "Logo" / >
         <
         /div>
