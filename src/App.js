cdd20821e6d21da3000afbb9bfc6533090590b54
sanamomin17 <sana.m.momin@outlook.com>
2021-10-16 21:06:54 +0500

    Container
    Made a Container and H1 inside it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return ( <
         div className = "App" >
-        LEARN REACT AND REACT NATIVE <
-        img src = "{https://pixabay.com/get/ge873844f34f4e47196679269a901b98ba85ca2ad6ee26512cf93f26df8f964e7a85a064237e8ee1c8b19756e4ba4e78f119d4916379cd090c757bf4fd39e4e31e1dfbbbcab6ebbea3f9a2c8dc757d64c_1920.jpg}"
-        alt = "Logo" / >
         <
+        div className = "container" >
+        <
+        h1 > Todo App < /h1> <
+        /div> <
         /div>
     );
 }
