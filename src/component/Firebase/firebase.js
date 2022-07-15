42030108e65464da5af98e127b25ef710e711374
sanamomin17 <sana.m.momin@outlook.com>
2022-07-15 14:28:47 +0500

    Update firebase.js

--- src/component/Firebase/firebase.js
+++ src/component/Firebase/firebase.js
@@ -1,10 +1,13 @@
 import { initializeApp } from "firebase/app";
 // import firebase from "firebase/app"; // * this means import everthig from firebase as "firebase"
-import { getFirestore } from "firebase/firestore/lite";
+// import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAP3PeLKd-oZ9UX0K9IC2dzH7yNP6LD1Ms",
     authDomain: "todo-app-e0f05.firebaseapp.com",
+    databaseURL: "https://todo-app-e0f05-default-rtdb.firebaseio.com",
     projectId: "todo-app-e0f05",
     storageBucket: "todo-app-e0f05.appspot.com",
     messagingSenderId: "661120805591",
@@ -16,4 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export default db;
+export { db };
